1a8ecf85ae6e54655503996a857bb4217682f761
TakashiSan <PeaceAndHiLight>
2020-02-07 18:18:59 +0900

    Initial Commit

---
+++ RGBBullsEye/TimeCounter.swift
@@ -0,0 +1,25 @@
+//
+//  TimerCount.swift
+//  RGBBullsEye
+//
+
+
+import Foundation
+import Combine
+
+class TimeCounter: ObservableObject {
+    var timer: Timer?
+    @Published var counter = 0
+    @objc func updateCounter() {
+        counter += 1
+    }
+    
+    init() {
+        timer = Timer.scheduledTimer(timeInterval:1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
